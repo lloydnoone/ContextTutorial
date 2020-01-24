620ae732e91a6ed0f5bc7eb3d3133ea18a6a4b32
lloydnoone <lloydnoone92@gmail.com>
2020-01-24 23:01:38 +0000

    context with class components

--- src/Form.js
+++ src/Form.js
@@ -15,42 +15,67 @@ import Select from '@material-ui/core/Select'
 import withStyles from '@material-ui/core/styles/withStyles'
 
 import styles from './styles/FormStyles'
+import { LanguageContext } from './contexts/LanguageContext'
+
+const words = {
+  english: {
+    signIn: 'Sign In',
+    email: 'Email Address',
+    password: 'Password',
+    remember: 'Remember Me'
+  },
+  french: {
+    signIn: 'Se Connecter',
+    email: 'Adresse Electronique',
+    password: 'Mot de Passe',
+    remember: 'Souviens-toi De Moi',
+  },
+  spanish: {
+    signIn: 'Registrarse',
+    email: 'Correo Electronico',
+    password: 'Contrasena',
+    remember: 'Recuerdame'
+  }
+}
 
 class Form extends Component {
+  static contextType = LanguageContext
   render() {
+    const { language, changeLanguage } = this.context
     const { classes } = this.props
+    const { email, signIn, password, remember } = words[language]
     return (
       <main className={classes.main}>
         <Paper className={classes.paper}>
           <Avatar className={classes.avatar}>
-            <LockOutlinedIcon/>
+            <LockOutlinedIcon />
           </Avatar>
-          <Typography variant='h5'>Sign In</Typography>
-          <Select value='english'>
+          <Typography variant='h5'>{signIn}</Typography>
+          <Select value={language} onChange={changeLanguage}>
             <MenuItem value='english' >English</MenuItem>
             <MenuItem value='french' >French</MenuItem>
             <MenuItem value='spanish' >Spanish</MenuItem>
           </Select>
           <form className={classes.form}>
             <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='email' >Email</InputLabel>
+              <InputLabel htmlFor='email' >{email}</InputLabel>
               <Input id='email' name='email' autoFocus ></Input>
             </FormControl>
             <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='password' >Password</InputLabel>
+              <InputLabel htmlFor='password' >{password}</InputLabel>
               <Input id='password' name='password' autoFocus ></Input>
             </FormControl>
-            <FormControlLabel 
-              label='Remember Me'
-              control={<Checkbox color='primary'/>}
+            <FormControlLabel
+              label={remember}
+              control={<Checkbox color='primary' />}
             />
-            <Button 
+            <Button
               variant="contained"
               type='submit'
               fullWidth
               color='primary'
-              className={ classes.submit }>
-                Sign In
+              className={classes.submit}>
+              {signIn}
             </Button>
           </form>
         </Paper>
--- src/Navbar.js
+++ src/Navbar.js
@@ -10,30 +10,53 @@ import Switch from '@material-ui/core/Switch'
 import { withStyles } from '@material-ui/core/styles'
 
 import styles from './styles/NavBarStyles'
+import { ThemeContext } from './contexts/ThemeContext'
+import { withLanguageContext } from './contexts/LanguageContext'
+
+const content = { 
+  english: {
+    search: 'Search'
+  },
+  french: {
+    search: 'Chercher'
+  },
+  spanish: {
+    search: 'Buscar'
+  }
+}
 
 class Navbar extends Component {
+  static contextType = ThemeContext
   render() {
+    const { isDarkMode, toggleTheme } = this.context
     const { classes } = this.props
+    const { language } = this.props.languageContext
+    const { search } = content[language]
     return (
       <div className={classes.root}>
-        <AppBar position='static' color='primary'>
+        <AppBar position='static' color={isDarkMode ? 'default' : 'primary'}>
           <ToolBar>
             <IconButton className={classes.menuButton} color='inherit'>
-              <span>French</span>
+              <span>Flag</span>
             </IconButton>
-            <Typography className={classes.title} variant='h6' color='inherit'>
+            <Typography
+              className={classes.title}
+              variant='h6'
+              color='inherit'
+            >
               App Title
             </Typography>
-            <Switch />
+            <Switch onChange={toggleTheme} />
             <div className={classes.grow} />
             <div className={classes.search}>
               <div className={classes.SearchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder='Search...' classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput
-              }}
+              <InputBase placeholder={`${search}...`}
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
               />
             </div>
           </ToolBar>
@@ -43,4 +66,4 @@ class Navbar extends Component {
   }
 }
 
-export default withStyles(styles)(Navbar)
+export default withLanguageContext(withStyles(styles)(Navbar))
